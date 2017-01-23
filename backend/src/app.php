398b5e4c48ef722b791626714ff08635eba9be99
Tim Roosen <tim.roosen@tamtam.nl>
2017-01-23 14:35:35 +0100

    added transition to status-bar change, added UTF8 as database encoding

--- backend/src/app.php
+++ backend/src/app.php
@@ -19,7 +19,8 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'password'  => 'root',
         'host'      => 'localhost',
         'driver'    => 'pdo_mysql',
-        'port'      => 3306
+        'port'      => 3306,
+        'charset' => 'utf8'
     ),
 ));
 
